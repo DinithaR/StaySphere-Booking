761df6a33b0d10d7e691cbd5a923d34998f74c37
DinithaR <dinitharanketh16@gmail.com>
2025-01-13 13:39:32 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Navigation from "./lib/Navigation";
+import Hero from "./lib/Hero";
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navigation />
+        <Hero />
+      </div>
+    </>
+  )
+}
+
+export default App
